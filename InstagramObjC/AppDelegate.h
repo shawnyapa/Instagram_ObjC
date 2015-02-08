b9bd0508f64da37f6c507d74e09d99010f5036f6
Shawn Yapa <shawnyapa@gmail.com>
2015-02-08 15:45:57 -0800

    Initial Commit

---
+++ InstagramObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramObjC
+//
+//  Created by Shawn Yapa on 2/8/15.
+//  Copyright (c) 2015 Shawn Yapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
