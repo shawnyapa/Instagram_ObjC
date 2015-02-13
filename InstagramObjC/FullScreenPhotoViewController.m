96dc73cafcd78cd6c710481f0236600f39ec8c22
Shawn Yapa <shawnyapa@gmail.com>
2015-02-12 22:36:02 -0800

    UI Update

--- InstagramObjC/FullScreenPhotoViewController.m
+++ InstagramObjC/FullScreenPhotoViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.navigationItem.title = @"Photo Zoom";
     self.photoView.image = self.photoImage;
     self.scrollView.contentSize = self.photoView.image.size;
 }
